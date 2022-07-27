538fbfaf72c87ec938743a81ea14ccea660cc1f7
Yin Lina <106166162+YinLina@users.noreply.github.com>
2022-07-28 00:46:05 +0700

    Add files via upload

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,20 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+
+    var x:Int=30
+//    if(x<20){
+//        print("x is lower than 20")
+//    }else{
+//        print("x is getter than 20")
+//    }
+    val result=if(x>20){
+        "x is getter than 20"
+    }else{
+        "x is lower than 20"
+    }
+    print(result)
+
+
+    val name:String?=null
+}
